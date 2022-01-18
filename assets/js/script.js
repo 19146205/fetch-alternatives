1a06e2461370a4ebbc94195b2f821537e0dbcdac
jibril yusuf <jibril_2012@live.com>
2022-01-18 20:49:01 +0000

    fixed

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// TODO: Edit the URL to get only 5 issues of Twitter's Chill repo
+const requestUrl = 'https://api.github.com/repos/twitter/chill/issues?per_page=5';
+
+async function getRequests() {
+  const response = await fetch(requestUrl)
+  const data = await response.json();
+  console.log('Github Repo Issues \n----------');
+  for (let i = 0; i < data.length; i++) {
+    const issue = data[i];
+    console.log(issue.url);
+    console.log(issue.user.login);
+  }
+}
+
+getRequests()
